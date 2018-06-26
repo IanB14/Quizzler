75f39308ff5bc4481a81c6ab45ca25a95322a1ab
Ian <ian_burke@hotmail.com>
2018-06-26 19:22:29 +0100

    V1.1
    - Updated the ViewController and AppDelegate files to remove unnecessary comments, add apppropriate comments and clean up the code a little

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,13 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
     let allQuestions = QuestionBank()
     var selectedAnswer: Bool = false
     var questionNumber: Int = 0
     var gameState: Int = 0
     var correctAnswers: Int = 0
-    
     var score: Int = 0
     
     
@@ -25,33 +23,39 @@ class ViewController: UIViewController {
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         nextQuestion()
-        
     }
 
 
+    // Core game logic - occurs when a button is tapped
     @IBAction func answerPressed(_ sender: AnyObject) {
+        
+        // Find out if the user tapped True or False
         if (sender.tag == 1){
             selectedAnswer = true
         }else if(sender.tag == 2){
             selectedAnswer = false
         }
         
+        // Check the users answer against the question
         checkAnswer()
         
+        // Increment the question number
         questionNumber += 1
         
+        // Display the next question
         nextQuestion()
         
     }
     
     
+    // This function updates UI elements such as the progress bar and score
     func updateUI() {
         scoreLabel.text = "Score: \(score)"
         progressLabel.text = "\(questionNumber + 1)/13"
-        
         progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
     
@@ -69,7 +73,6 @@ class ViewController: UIViewController {
                                               handler: {(UIAlertAction) in self.startOver()})
             
             alert.addAction(restartAction)
-            
             present(alert, animated: true, completion: nil)
         }
     }
@@ -80,7 +83,6 @@ class ViewController: UIViewController {
             ProgressHUD.showSuccess("Correct")
             correctAnswers += 1
             score += 1
-            
         }else{
             ProgressHUD.showError("Incorrect")
         }
